979fda1b5c0512085468ba9f64961f1a01f2df1d
bienthehumanoid <bienvenidojr.mendoza@gmail.com>
2026-08-03 18:05:11 +0800

    task: scaffold the proctor software (#6)

---
+++ src/PhilSLA.ExamPlatform.Proctor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace PhilSLA.ExamPlatform.Proctor;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "PhilSLA.ExamPlatform.Proctor" };
+	}
+}
---
+++ src/PhilSLA.ExamPlatform.Proctor/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PhilSLA.ExamPlatform.Proctor;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
